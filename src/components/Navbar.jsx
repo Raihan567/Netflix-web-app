e677a83d4c27c4c8ea71b729a7a54db0565449dc
Raihan567 <aburaihansarker567@gmail.com>
2022-08-15 14:52:36 +0600

    modified navbar;

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+  console.log(user?.email);
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="flex justify-between items-center p-4 sticky top-0 w-full z-[100] bg-black ">
       <Link to="/">
@@ -9,16 +23,30 @@ const Navbar = () => {
           Netflix
         </h1>
       </Link>
-      <div>
-        <Link to="/login">
-          <button className="mr-4 text-white ">Sign In</button>
-        </Link>
-        <Link to="/signup">
-          <button className="bg-red-600 text-white py-2 px-6 rounded-md ">
-            Sign Up
+      {user?.email ? (
+        <div>
+          <Link to="/account">
+            <button className="text-white pr-4">Account</button>
+          </Link>
+          <button
+            onClick={handleLogout}
+            className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white"
+          >
+            Logout
           </button>
-        </Link>
-      </div>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="mr-4 text-white ">Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 text-white py-2 px-6 rounded-md ">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
